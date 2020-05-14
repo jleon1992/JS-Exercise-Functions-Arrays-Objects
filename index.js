01e5dfa84e617c41ebcd7e2e5c5986c961b29374
jleon1992 <36936276+jleon1992@users.noreply.github.com>
2020-05-14 08:27:02 -0400

    make person object

--- index.js
+++ index.js
@@ -106,9 +106,15 @@ console.log(temperatureInF(24, "C")) ;
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  let person = {
+    id: id,
+    name: name,
+    email: email
+  };
+  return person;
 }
+console.log(makePersonObject(5, "leia", "leia@leia.com"));
 
 /**
  * ### Challenge `getName`
